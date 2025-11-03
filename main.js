82c3f908723ba4945834ffa7e143efcc419be048
sofilarrea <sofialejandra.larrea@gmail.com>
2025-11-03 18:39:04 -0300

    navbar final

--- main.js
+++ main.js
@@ -4,18 +4,17 @@ gsap.registerPlugin(ScrollTrigger);
 /* ✅ PRELOADER + HERO */
 window.addEventListener("load", () => {
   const tl = gsap.timeline({ defaults: { ease: "power3.out" } });
+tl.from("#preloader-box", { opacity: 0, y: 50, duration: 0.8 })
+  .to("#preloader-bg", { scale: 20, duration: 1.3, ease: "power4.in" }) // ✅ escala solo fondo
+  .to("#loader", {
+    opacity: 0,
+    duration: 0.6,
+    onComplete: () => {
+      document.getElementById("loader").style.display = "none";
+      document.body.style.overflow = "auto";
+    }
+  })
 
-  tl.from("#preloader-box", { opacity: 0, y: 50, duration: 0.8 })
-    .to("#preloader-box", { duration: 1 })
-    .to("#preloader-box", { scale: 20, duration: 1.3, ease: "power4.in" })
-    .to("#loader", {
-      opacity: 0,
-      duration: 0.6,
-      onComplete: () => {
-        document.getElementById("loader").style.display = "none";
-        document.body.style.overflow = "auto";
-      }
-    })
     .from("#hero", { opacity: 0, duration: 1 }, "-=0.3")
     .from(".hero__title", { opacity: 0, y: 40, duration: 1 }, "-=0.2")
     .from(".hero__subtitle", { opacity: 0, y: 40, duration: 1 }, "-=0.8");
